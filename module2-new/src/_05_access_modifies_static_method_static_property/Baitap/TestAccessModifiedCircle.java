984da6ac58883331ad5813d846fc9f32b27b5da0
duchiep154 <duchiep154@gmail.com>
2020-09-16 08:10:56 +0700

    up bai moi

---
+++ module2-new/src/_05_access_modifies_static_method_static_property/Baitap/TestAccessModifiedCircle.java
@@ -0,0 +1,9 @@
+package _05_access_modifies_static_method_static_property.Baitap;
+
+public class TestAccessModifiedCircle {
+    public static void main(String[] args) {
+        AccessModifiedCircle circle1 = new AccessModifiedCircle();
+//        circle1.getRadius(10);
+        System.out.println("Bán kính hình tròn: " + circle1.getRadius()+ "\n" + "Diện tích hình tròn: " + circle1.getArea());
+    }
+}
