535d9564fcc070945aebca7818b76a630230e897
duchiep154 <duchiep154@gmail.com>
2020-09-29 11:05:46 +0700

    up bai moi

--- module2-new/src/_14_thuat_toan_sap_xep/bai_tap/minh_hoa_sap_xep_chen/SapXepChen.java
+++ module2-new/src/_14_thuat_toan_sap_xep/bai_tap/minh_hoa_sap_xep_chen/SapXepChen.java
@@ -4,10 +4,10 @@ public class SapXepChen {
     public void insertionSort(int arr[]) {
         int giaTriChen;
         int viTriChen;
-        int index;
+
 
         // lap qua tat ca cac so
-        for (index = 1; index < arr.length; index++) {
+        for (int index = 1; index < arr.length; index++) {
 
             // chon mot gia tri de chen
             giaTriChen = arr[index];
@@ -35,11 +35,11 @@ public class SapXepChen {
     }
 
     public void display(int arr[]) {
-        int i;
+
         System.out.print("[");
 
         // Duyet qua tat ca phan tu
-        for (i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
@@ -48,12 +48,12 @@ public class SapXepChen {
 
     public static void main(String[] args) {
         // khoi tao mang arr
-        int arr[] = { 6, 7, 0, 2, 8, 1, 3, 9, 4, 5 };
+        int arr[] = { 6, 7, 0, 2, 8, 1, 3, 9, 4, 5,18,31,74,69,58,20,14,16};
 
         SapXepChen sapXepChen = new SapXepChen();
         System.out.println("Mang du lieu dau vao: ");
         sapXepChen.display(arr);
-        System.out.println("*****************************");
+        System.out.println("-----------------------------");
         sapXepChen.insertionSort(arr);
         System.out.println("-----------------------------");
         System.out.println("\nMang sau khi da sap xep: ");
